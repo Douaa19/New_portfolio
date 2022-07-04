2f8809cc0348ffa13a333eb6ba722b17d968e812
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-07-04 01:10:07 +0100

    createFilesRouters

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const app = express();
 const morgan = require("morgan");
 const PORT = process.env.PORT || 8080;
 
-//
+// require mongoose
+require("./src/config/mongoose");
+
+// using middlewares
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +20,11 @@ app.get("/", (req, res) => {
   });
 });
 
+// routes
+
+// listen to the port
 app.listen(PORT, () => {
   console.log(`server is running in: http://localhost:${PORT}`);
 });
+
+module.exports = app;
