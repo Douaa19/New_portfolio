6a44b33d8f54aa6e48238ca912debf108e3144f1
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-09-16 13:08:45 +0100

    myProjectsSection

--- portfolio_front/src/components/my-projects/Crads.js
+++ portfolio_front/src/components/my-projects/Crads.js
@@ -24,6 +24,7 @@ function Crads() {
       minHeight: "19rem",
       maxHeight: "22rem",
       padding: "2rem 2.5rem",
+      marginBottom: "1.5rem",
     },
     title: {
       color: "#676FA3",
@@ -42,10 +43,107 @@ function Crads() {
     },
   };
 
-
   return (
     <CardGroup>
       <Row style={styles.row}>
+        <Col sm="5">
+          <Card body style={styles.card}>
+            <CardTitle tag="h5" style={styles.title}>
+              Project 1
+            </CardTitle>
+            <h5 style={styles.smallTitle}>technologies</h5>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incidid
+            </CardText>
+          </Card>
+        </Col>
+        <Col sm="5">
+          <Card body style={styles.card}>
+            <CardTitle tag="h5" style={styles.title}>
+              Project 1
+            </CardTitle>
+            <h5 style={styles.smallTitle}>technologies</h5>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incidid
+            </CardText>
+          </Card>
+        </Col>
+        <Col sm="5">
+          <Card body style={styles.card}>
+            <CardTitle tag="h5" style={styles.title}>
+              Project 1
+            </CardTitle>
+            <h5 style={styles.smallTitle}>technologies</h5>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incidid
+            </CardText>
+          </Card>
+        </Col>
+        <Col sm="5">
+          <Card body style={styles.card}>
+            <CardTitle tag="h5" style={styles.title}>
+              Project 1
+            </CardTitle>
+            <h5 style={styles.smallTitle}>technologies</h5>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incidid
+            </CardText>
+          </Card>
+        </Col>
+        <Col sm="5">
+          <Card body style={styles.card}>
+            <CardTitle tag="h5" style={styles.title}>
+              Project 1
+            </CardTitle>
+            <h5 style={styles.smallTitle}>technologies</h5>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incidid
+            </CardText>
+          </Card>
+        </Col>
+        <Col sm="5">
+          <Card body style={styles.card}>
+            <CardTitle tag="h5" style={styles.title}>
+              Project 1
+            </CardTitle>
+            <h5 style={styles.smallTitle}>technologies</h5>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incidid
+            </CardText>
+          </Card>
+        </Col>
+        <Col sm="5">
+          <Card body style={styles.card}>
+            <CardTitle tag="h5" style={styles.title}>
+              Project 1
+            </CardTitle>
+            <h5 style={styles.smallTitle}>technologies</h5>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incidid
+            </CardText>
+          </Card>
+        </Col>
         <Col sm="5">
           <Card body style={styles.card}>
             <CardTitle tag="h5" style={styles.title}>
