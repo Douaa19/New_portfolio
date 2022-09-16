03e540ffb08e1079fa2742a52f6af8e271c3b079
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-09-16 17:42:54 +0100

    cardsInfos

--- portfolio_front/src/components/contact/Cards.js
+++ portfolio_front/src/components/contact/Cards.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import { Card, CardGroup, CardTitle, CardText, Row, Col } from "reactstrap";
 
 function Cards() {
+  // styles
+  const styles = {
+    row: {
+      display: "flex",
+      justifyContent: "center",
+    },
+    card: {
+      background: "none",
+      color: "#FFF",
+      border: "1px solid #FF5959",
+      minHeight: "8rem",
+      maxHeight: "22rem",
+      padding: "1rem 1rem",
+      width: "25rem",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-around",
+    },
+    title: {
+      color: "#FFF",
+      fontWeight: "600",
+      fontSize: "18px",
+    },
+    text: {
+      color: "#FFF",
+      fontSize: "16px",
+      display: "flex",
+      flexDirection: "column",
+    },
+  };
+
   return (
-    <div>Cards</div>
-  )
+    <>
+      <CardGroup>
+        <Row style={styles.row}>
+          <Col sm="4">
+            <Card body style={styles.card}>
+              <CardTitle tag="h5" style={styles.title}>
+                location
+              </CardTitle>
+              <CardText style={styles.text}>
+                <span>morocco, youssoufia </span>
+                <span>street abdessamad el kanfaoui n°12 </span>
+              </CardText>
+            </Card>
+          </Col>
+          <Col sm="4">
+            <Card body style={styles.card}>
+              <CardTitle tag="h5" style={styles.title}>
+                email
+              </CardTitle>
+              <CardText style={styles.text}>doua.larif@gmail.com</CardText>
+            </Card>
+          </Col>
+          <Col sm="4">
+            <Card body style={styles.card}>
+              <CardTitle tag="h5" style={styles.title}>
+                phone
+              </CardTitle>
+              <CardText style={styles.text}>+212 640731248</CardText>
+            </Card>
+          </Col>
+        </Row>
+      </CardGroup>
+    </>
+  );
 }
 
-export default Cards
+export default Cards;
