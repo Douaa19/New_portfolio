5e381a40d8bd7b1a5070228d60fff1b9a59ad028
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-09-19 14:36:20 +0100

    exportAllRoutes

--- portfolio_back/src/routes/index.js
+++ portfolio_back/src/routes/index.js
@@ -1,5 +1,15 @@
 const projectsRoutes = require("./projectsRoutes");
+const servicesRoutes = require("./servicesRoutes");
+const clientsRoutes = require("./clientsRoutes");
+const partsRoutes = require("./partsRoutes");
+const technossRoutes = require("./technossRoutes");
+const authRoutes = require("./authRoutes");
 
 module.exports = {
   projectsRoutes,
+  servicesRoutes,
+  clientsRoutes,
+  partsRoutes,
+  technossRoutes,
+  authRoutes,
 };
