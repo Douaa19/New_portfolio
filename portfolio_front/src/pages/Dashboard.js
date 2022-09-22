74a1d153af91bafd3bce356451cef6fe14f779ec
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-09-23 00:56:24 +0100

    séparerLesComposants

--- portfolio_front/src/pages/Dashboard.js
+++ portfolio_front/src/pages/Dashboard.js
@@ -1,22 +1,15 @@
 import React from "react";
-import { Container, Button, Card, Col, Row } from "reactstrap";
+import { Container } from "reactstrap";
+import Sections from "../components/dashboard/Sections";
+import Projects from "../components/dashboard/panels/Projects";
 
 function Dashboard() {
-  const myObj = [
-    "projects",
-    "services",
-    "tachnologies",
-    "part",
-    "informations",
-    "clinets",
-  ];
-
   // styles
   const styles = {
     body: {
       backgroundColor: "#101010",
       width: "100%",
-      height: "100%",
+      height: "100vh",
     },
     container: {
       padding: "1rem 0",
@@ -32,42 +25,15 @@ function Dashboard() {
       letterSpacing: "4px",
       marginBottom: "2rem",
     },
-    card: {
-      border: "none",
-      margin: "0 0 1rem",
-    },
-    btn: {
-      border: "none",
-      padding: "0.8rem 0",
-      textTransform: "uppercase",
-      fontFamily: "'Montserrat', sans-serif",
-      letterSpacing: "2px",
-      background: "#676FA3",
-      width: "100%"
-    },
-    link: {
-      color: "#FFFFFF",
-      textDecoration: "none",
-      fontSize: "18px",
-    },
   };
   return (
     <div className="" style={styles.body}>
       <Container style={styles.container}>
         <h2 style={styles.title}>dashboard</h2>
-        <Row>
-          {myObj.map((element, index) => {
-            return (
-              <Col sm="3" key={index}>
-                <Card style={styles.card}>
-                  <a href="" style={styles.link}>
-                    <Button style={styles.btn}>{element}</Button>
-                  </a>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
+        <Sections />
+        {/* Panels */}
+        <Projects />
+        {/* Panels */}
       </Container>
     </div>
   );
