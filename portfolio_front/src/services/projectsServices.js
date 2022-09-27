6f02bf520cc89b80cacd1409f1f1ee352ad293b9
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-09-27 12:01:20 +0100

    deleteService&ProjectInServicesFolder

--- portfolio_front/src/services/projectsServices.js
+++ portfolio_front/src/services/projectsServices.js
@@ -3,15 +3,26 @@ const PROJECTS_URL = "http://localhost:8080/projects/";
 
 // add project
 export async function addProject(data, header) {
-    await axios
-      .post(`${PROJECTS_URL}`,data, {
-        headers: {
-          Authorization: `Bearer ${header}`,
-        },
-      })
-      .then((response) => {
-        return response;
-      });
-  }
+  await axios
+    .post(`${PROJECTS_URL}`, data, {
+      headers: {
+        Authorization: `Bearer ${header}`,
+      },
+    })
+    .then((response) => {
+      return response;
+    });
+}
 
-
+// delete project
+export async function deleteProject(id, header) {
+  await axios
+    .delete(`${PROJECTS_URL}${id}`, {
+      headers: {
+        Authorization: `Bearer ${header}`,
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+    });
+}
--- portfolio_front/src/services/services.js
+++ portfolio_front/src/services/services.js
@@ -4,12 +4,25 @@ const SERVICES_URL = "http://localhost:8080/services/";
 // add service
 export async function addService(data, header) {
   await axios
-    .post(`${SERVICES_URL}`,data, {
+    .post(`${SERVICES_URL}`, data, {
       headers: {
         Authorization: `Bearer ${header}`,
       },
     })
     .then((response) => {
       return response.data;
+    });
+}
+
+// delete service
+export async function deleteService(id, header) {
+  await axios
+    .delete(`${SERVICES_URL}${id}`, {
+      headers: {
+        Authorization: `Bearer ${header}`,
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
     });
 }
