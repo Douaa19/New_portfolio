17387b03c9ccda08e507e72b3f849efe3b101eb9
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-09-27 11:57:44 +0100

    addProject

--- portfolio_front/src/components/dashboard/panels/projects/AddForm.js
+++ portfolio_front/src/components/dashboard/panels/projects/AddForm.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Form, Button, FormGroup, Input } from "reactstrap";
+import { addProject } from "../../../../services/projectsServices";
 
 function AddForm() {
+  const header =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMjg5MzIzMzkxMTIyNWJmZjBkNGQ2MSIsImVtYWlsIjoiZG91YS5sYXJpZkBnbWFpbC5jb20iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NjQyNzI1NTB9.YHatDqPv3bm4Ioejlwz16U-1zQ4x17kMoD4aLh1Grtk";
+  //
+  const [values, setValues] = useState({
+    project_name: "",
+    // technologies: [],
+    description: "",
+    link: "",
+  });
+
+  // handles
+  const handleName = (e) => {
+    setValues({ ...values, project_name: e.target.value });
+  };
+  // const handleTechnos = (e) => {
+  //   setValues({ ...values, technologies: e.target.value });
+  // };
+  const handleDescription = (e) => {
+    setValues({ ...values, description: e.target.value });
+  };
+  const handleLink = (e) => {
+    setValues({ ...values, link: e.target.value });
+  };
+
+  // handle submit function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (values && header) {
+      await addProject(values, header).then((response) => {
+        window.location = "/dashboard";
+      });
+    }
+  };
+
   // styles
   const styles = {
     container: {
@@ -44,13 +79,15 @@ function AddForm() {
         <div className="title text-center">
           <h4 style={styles.title}>Add project</h4>
         </div>
-        <Form style={styles.form}>
+        <Form style={styles.form} method="POST" onSubmit={handleSubmit}>
           <FormGroup className="mb-3">
             <Input
               type="text"
               placeholder="project name"
               style={styles.input}
               nmae="project_name"
+              value={values.project_name}
+              onChange={handleName}
             />
           </FormGroup>
           <FormGroup className="mb-3">
@@ -59,10 +96,18 @@ function AddForm() {
               type="textarea"
               placeholder="description"
               style={styles.input}
+              value={values.description}
+              onChange={handleDescription}
             />
           </FormGroup>
           <FormGroup className="mb-3">
-            <Input type="url" placeholder="link project" style={styles.input} />
+            <Input
+              type="url"
+              placeholder="link project"
+              style={styles.input}
+              value={values.link}
+              onChange={handleLink}
+            />
           </FormGroup>
           <FormGroup className="mb-3">
             <Input type="checkbox" placeholder="technologies" />
--- portfolio_front/src/services/projectsServices.js
+++ portfolio_front/src/services/projectsServices.js
@@ -1,4 +1,17 @@
 import axios from "axios";
 const PROJECTS_URL = "http://localhost:8080/projects/";
 
+// add project
+export async function addProject(data, header) {
+    await axios
+      .post(`${PROJECTS_URL}`,data, {
+        headers: {
+          Authorization: `Bearer ${header}`,
+        },
+      })
+      .then((response) => {
+        return response;
+      });
+  }
+
 
