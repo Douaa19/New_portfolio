3ed5da3344d2a241ac239bc416ecc4b5ca670979
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-11-14 22:25:34 +0100

    removeBackend

--- portfolio_back/src/config/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1/PORTFOLIO", () => {
-  console.log(`database connected successfully!`, (e) =>
-    console.log(e.message)
-  );
-});
-
-module.exports = mongoose;
